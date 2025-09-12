740ac6cb4fa5547a077208ffa542afe911af331e
Nicolas Mendes Fernandes <nicolasmendes@ufpr.br>
2025-09-12 17:28:19 -0300

    Render Configuration

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@
 set -e
 
 # Roda as migrações do banco de dados
+composer install --no-dev
+npm install
+npm run build
+php artisan optimize:clear
 php artisan migrate --force
 
 # Executa o comando original para iniciar o servidor web.
