9fa824d8a682963e7652c8cafc664d7b2e42e382
Nicolas Mendes Fernandes <nicolasmendes@ufpr.br>
2025-09-25 16:00:08 -0300

    version 01

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,10 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
-        
+
         Model::unguard();
 
         Password::defaults(function () {
