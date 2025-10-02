b4d5f9bd4506a89f98c3e6a47b1bd19ed5089039
Nicolas Mendes Fernandes <nicolasmendes@ufpr.br>
2025-10-02 14:42:17 -0300

    Fix JS files import in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/auth'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/auth/fileVerification.js','resources/js/auth/passwordVerification.js'],
             refresh: true,
         }),
         tailwindcss(),
